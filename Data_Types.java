69dd48dc023cc58d87f6b2c625a814c3011e5cad
KumarTamilan <kumaranharsh123@gmail.com>
2023-12-17 22:08:16 +0530

    My Data Type

---
+++ Data_Types.java
@@ -0,0 +1,48 @@
+package com.datatypes;
+
+public class Data_Types {
+
+	public static void main (String[ ]  args) {
+		
+	byte a = 100;
+	byte a1 = 120;
+	System.out.println("Byte a value is"+" "+a+("\n")+"Byte a1 value is"+" "+a1);
+	System.out.println();
+	
+	short b = 10000;
+	short b1 = 20000;
+	System.out.println("Short b value is"+" "+b+("\n")+"Short b1 value is"+" "+b1);
+	System.out.println();
+	
+	int c = 100000000;
+	int c1 = 200000000;
+	System.out.println("Int c value is"+" "+c+("\n")+"Int c1 value is"+" "+c1);
+	System.out.println();
+	
+	long d = 1000000000;
+	long d1 = 2000000000;
+	System.out.println("Lona d value is"+" "+d+("\n")+"Long d1 value is"+" "+d1);
+	System.out.println();
+	
+	float e = 0.0000000000000008f;
+	float e1 = 0.0000000000000005f;
+	System.out.println("Float e value is"+" "+e+("\n")+"Float e1 value is"+" "+e1);
+	System.out.println();
+	
+	double f = 100000000;
+	double f1 = 200000000;
+	System.out.println("Double f value is"+" "+f+("\n")+"Double f1 value is"+" "+f1);
+	System.out.println();
+	
+	char g ='Z';
+	char g1 = 'X';
+	System.out.println("Char g value is"+" "+g+("\n")+"Char g1 value is"+" "+g1);
+	System.out.println();
+	
+	boolean h = true;
+	boolean h1 = false;
+	System.out.println("Boolean h value is"+" "+h+("\n")+"Boolean h1 value is"+" "+h1);
+	
+	
+	}
+}
